6e9ec14feb10abbae5159d4549633ef9e9251c53
Sushant1244 <189602153+Sushant1244@users.noreply.github.com>
2025-06-04 15:27:42 +0545

    update

---
+++ src/Controller/NewClass.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+/**
+ *
+ * @author sumitshah
+ */
+public class NewClass {
+
+    public static class CustomerPersonalInformation {
+
+        public CustomerPersonalInformation() {
+        }
+
+        public void show() {
+            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        }
+    }
+    
+}
